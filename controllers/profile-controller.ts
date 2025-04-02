90a965360c11e1e7a194c3af392fb313c535d867
Kevin Julian Alvarez Ramirez <lukitor1414@gmail.com>
2025-04-01 22:43:38 -0500

    hecho la funcion de verifyToken y su prueba

--- controllers/profile-controller.ts
+++ controllers/profile-controller.ts
@@ -3,9 +3,9 @@ import { Request, Response } from "express";
 
 let profile = async (req: Request, res: Response) => {
   try {    
-    const idUser = req.body.id;
+    const id = req.body.id;
     return res.status(200).json(
-      { status: 'Get profile Ok', id: idUser }
+      { status: 'Get profile Ok', id: id }
     );
   } catch (error: any) {
     return res.status(500).json({ errorInfo: "An unknown error has occurred" }
