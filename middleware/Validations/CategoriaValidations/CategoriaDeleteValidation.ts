da22a079568fb49e433c4fb9173713c9e61c49e0
David Steven T <davidtoro013125@gmail,com>
2025-05-06 15:11:11 -0500

    validations categoria

--- middleware/Validations/CategoriaValidations/CategoriaDeleteValidation.ts
+++ middleware/Validations/CategoriaValidations/CategoriaDeleteValidation.ts
@@ -2,10 +2,7 @@ import { check, validationResult, ValidationChain } from 'express-validator';
 import { Request, Response, NextFunction } from 'express';
 
 // Validaciones para el registro de un administrador
-export const categoriaUpdateValidation: ValidationChain[] = [
-  check('new_nombre_categoria')
-    .isLength({ min: 1, max: 255 })
-    .withMessage('El campo "nuevo nombre categoria" es obligatorio y debe tener entre 1 y 255 caracteres.'),
+export const categoriaDeleteValidation: ValidationChain[] = [
   check('nombre_categoria')
     .isLength({ min: 1, max: 255 })
     .withMessage('El campo "nombre categoria" es obligatorio y debe tener entre 1 y 255 caracteres.'),
@@ -13,7 +10,7 @@ export const categoriaUpdateValidation: ValidationChain[] = [
 
 
 // Middleware para manejar errores de validación
-export function validateCategoriaUpdate(req: Request, res: Response, next: NextFunction) {
+export function validateCategoriaDelete(req: Request, res: Response, next: NextFunction) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
