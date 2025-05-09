78814026509423ef8b227085f35375f9a4fb113a
luiscar2050 <luis3206162549@gmail.com>
2025-05-09 17:21:19 -0500

    modificacion al controlador del tecnico

--- controllers/TecnicoController/TecnicoGetController.ts
+++ controllers/TecnicoController/TecnicoGetController.ts
@@ -3,22 +3,26 @@ import TecnicoServices from "../../services/TecnicoServices";
 
 let TecnicoGet = async (req: Request, res: Response) => {
   try {
-    const {
-      correo_tecnico
-    } = req.query;
-    const getTecnico = await TecnicoServices.getbyEmail(correo_tecnico as string)
-    return res.status(201).json(
-      { status: 'Consult ok',
-        data: getTecnico
-      }
-    );
-  } catch (error: any) {
-    if (error && error.code == "ER_DUP_ENTRY") {
-      return res.status(500).json({ errorInfo: error.sqlMessage }
-      );
-    }
-  }
-}
+    const { correo_tecnico } = req.query;
+    const getTecnico = await TecnicoServices.getbyEmail(correo_tecnico as string);
 
+    if (Array.isArray(getTecnico) && getTecnico.length > 0) {
+      const Tecnico = getTecnico[0] as { imagen: Buffer };
+
+      res.status(200).json({
+        status: 'Consult ok',
+        data: {
+          ...Tecnico,
+          imagen: Tecnico.imagen.toString('base64'), // conversión directa
+        },
+      });
+    } else {
+      res.status(404).json({ error: 'Tecnico no encontrado' });
+    }
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ error: 'Error del servidor' });
+  }
+};
 
 export default TecnicoGet;
