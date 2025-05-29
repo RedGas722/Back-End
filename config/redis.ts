102fcf4e06b20108edb930dcc5a7df14e3108113
Kevin Julian Alvarez Ramirez <lukitor1414@gmail.com>
2025-05-28 23:18:05 -0500

    modificacion para usar redis

--- config/redis.ts
+++ config/redis.ts
@@ -1,10 +1,6 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT) || 6379,
-  retryStrategy: () => null // para no reconectar automáticamente si quieres
-});
+export const redis = new Redis(process.env.REDIS_URL!);
 
 redis.on('error', (err) => {
   console.warn('[Redis]', err.message);
