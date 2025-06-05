fab9a242f4eafbf0945bf53b56f1ba23fa687f56
Kevin Julian Alvarez Ramirez <lukitor1414@gmail.com>
2025-06-05 00:28:29 -0500

    x

--- repositories/SeEncuentraRepository.ts
+++ repositories/SeEncuentraRepository.ts
@@ -30,7 +30,7 @@ class SeEncuentraRepository {
     const sqlDelete = `
       DELETE FROM se_encuentra
       WHERE id_producto = ? AND id_categoria IN (
-        SELECT id_categoria FROM categoria WHERE nombre != ?
+        SELECT id_categoria FROM categoria WHERE nombre_categoria != ?
       )
     `;
     await db.execute(sqlDelete, [id_producto, 'Ofertas']);
