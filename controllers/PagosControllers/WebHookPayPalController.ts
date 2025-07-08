b16d96bb1928f2e407a3da2ee56805bbc3080df1
Kevin Julian Alvarez Ramirez <lukitor1414@gmail.com>
2025-07-08 11:04:33 -0500

    eliminacion de consoles en webhook de paypal

--- controllers/PagosControllers/WebHookPayPalController.ts
+++ controllers/PagosControllers/WebHookPayPalController.ts
@@ -3,14 +3,9 @@ import PagoPaypalServices from "../../services/PagosServices/PagoPaypalServices"
 
 const WebhookPaypalController = async (req: Request, res: Response) => {
   try {
-    console.log("📩 Webhook recibido en PayPal");
-    console.log("Raw body:", JSON.stringify(req.body, null, 2));
-
     const body = req.body;
     const eventType = body.event_type;
     const resource = body.resource;
-
-    console.log("🔔 Evento recibido:", eventType);
 
     // Usar el evento CHECKOUT.ORDER.APPROVED
     if (eventType === "CHECKOUT.ORDER.APPROVED") {
@@ -19,9 +14,6 @@ const WebhookPaypalController = async (req: Request, res: Response) => {
       const referencia =
         resource?.purchase_units?.[0]?.custom_id || `REF-${Date.now()}`;
       const cantidad = resource?.purchase_units?.[0]?.amount?.value;
-
-      console.log("📌 Estado de la orden:", status);
-      console.log("📧 Email:", email, "💲 Cantidad:", cantidad);
 
       if (!email || !cantidad) {
         throw new Error("Datos insuficientes en el webhook");
