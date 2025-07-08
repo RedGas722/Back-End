d974f917c9b16f39657b69c6ed431f0bab28b661
Kevin Julian Alvarez Ramirez <lukitor1414@gmail.com>
2025-07-07 22:28:04 -0500

    modificacion a generate token para que sea global

--- controllers/TokenControllers/GenerateTokenRecoveryController.ts
+++ controllers/TokenControllers/GenerateTokenRecoveryController.ts
@@ -5,22 +5,14 @@ const SECRET = process.env.KEY_RECOVERY || "clave_temporal";
 
 const GenerateTokenRecovery = async (req: Request, res: Response) => {
   try {
-    const { correo_cliente } = req.body;
+    const { correo } = req.body;
 
-    if (!correo_cliente) {
+    if (!correo) {
       return res.status(400).json({ error: "Correo requerido" });
     }
 
-    // Validar si el correo existe
-    const clienteRes = await fetch(`https://redgas.onrender.com/ClienteGet?correo_cliente=${correo_cliente}`);
-    const clienteData = await clienteRes.json();
-
-    if (!clienteData?.data?.id_cliente) {
-      return res.status(404).json({ error: "Correo no registrado" });
-    }
-
     // Generar token válida por 10 minutos
-    const token = generateToken({ correo: correo_cliente }, SECRET, 10);
+    const token = generateToken({ correo: correo }, SECRET, 10);
 
     return res.status(200).json({ token });
   } catch (error: any) {
